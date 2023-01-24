f93b8cfce7bb95f10e40cb699eaf7679bb54e1b4
fickkmdev <fickkm.dev@gmail.com>
2023-01-24 09:18:59 +0700

    add middleware

--- main.go
+++ main.go
@@ -4,26 +4,38 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/gin-gonic/gin"
 	"github.com/fickkmdev/backend-golang101/model"
+	"github.com/gin-gonic/gin"
 )
 
-func main()  {
+func main() {
 	r := gin.Default()
-	r.GET("/welcome", welcome)
-	r.POST("/welcome-message", welcomeMessage)
-	r.POST("/welcome-message-json", welcomeMessageJson)
+
+	//middleware ต้องทำในmiddlewareก่อน ถึงจะทำโค้ดทุกบรรทัดหลังจากนี้
+	r.Use(middlewareFunc())
+	{
+		r.GET("/welcome", welcome)
+		r.POST("/welcome-message", welcomeMessage)
+		r.POST("/welcome-message-json", welcomeMessageJson)
+	}
 
 	r.Run(":3030")
 }
 
-func welcome(ctx *gin.Context){
+func middlewareFunc() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		//middleware
+		fmt.Println("middleware doing...")
+	}
+}
+
+func welcome(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{
 		"message": "hello, world",
 	})
 }
 
-func welcomeMessage(ctx *gin.Context){
+func welcomeMessage(ctx *gin.Context) {
 	var result model.Result
 	result.ID = ctx.Query("id")
 	result.Name = ctx.PostForm("name")
@@ -42,8 +54,8 @@ func welcomeMessageJson(ctx *gin.Context) {
 	ctx.JSON(
 		http.StatusOK,
 		gin.H{
-			"id": input.ID,
-			"name": input.Name,
+			"id":      input.ID,
+			"name":    input.Name,
 			"message": input.Message,
 		},
 	)
